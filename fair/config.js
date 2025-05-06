d8867599709398f67c0bbde504aca99d841de4e7
OT-Lyz <150920750+OT-Lyz@users.noreply.github.com>
2025-05-06 23:21:59 +0800

    Update config.js

--- fair/config.js
+++ fair/config.js
@@ -1,2 +1,2 @@
-export const API_KEY = 'sk-e74bd7de259542b88c79847716a5f3ea';
-export const API_URL = 'https://api.deepseek.com/v1/chat/completions';
+export const API_KEY = 'sk-5b88e81435de4369bd68a2fe73b42d77';  
+export const API_URL = 'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions';
