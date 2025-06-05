621515bbee5d4276dae005c30d09df5c64361674
OT-Lyz <150920750+OT-Lyz@users.noreply.github.com>
2025-06-05 20:39:14 +0800

    Update config.js

--- fair/config.js
+++ fair/config.js
@@ -1,2 +1,2 @@
-export const API_KEY = 'sk-5b88e81435de4369bd68a2fe73b42d77';  
+export const API_KEY = '';  
 export const API_URL = 'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions';
